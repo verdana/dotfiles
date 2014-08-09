e6672c4b6922a0e56a65782d51d01fe410e8e209
Verdana Mu <verdana.cn@gmail.com>
2014-08-09 16:25:23 +0800

    平台脚本分拆到 platform 目录中

--- .zsh/platform.zsh
+++ .zsh/platform.zsh
@@ -1,12 +1,14 @@
 
 # 平台差异
 case $( uname -s ) in
+    CYGWIN*)
+        source $zsh/platform/cygwin.zsh ;;
     Darwin)
-        source $zsh/platform_mac.zsh ;;
+        source $zsh/platform/mac.zsh ;;
     FreeBSD)
-        source $zsh/platform_freebsd.zsh ;;
+        source $zsh/platform/freebsd.zsh ;;
     Linux)
-        source $zsh/platform_linux.zsh ;;
+        source $zsh/platform/linux.zsh ;;
 esac
 
 # vim: set fdm=marker ff=unix sw=4 ts=4 et:
---
+++ .zsh/platform/cygwin.zsh
@@ -0,0 +1,6 @@
+
+# Cygwin
+alias ls='ls --human-readable --color=auto'
+alias l='ls -lhA'
+alias ll='ls -lhA'
+
