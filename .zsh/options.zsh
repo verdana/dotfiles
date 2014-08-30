0df4e8d187f113f869372793404e1032f59e34f7
Verdana Mu <verdana.cn@gmail.com>
2014-08-30 18:24:17 +0800

    去掉命令纠错功能，烦！

--- .zsh/options.zsh
+++ .zsh/options.zsh
@@ -3,7 +3,6 @@
 for option (
     autocd                  # 切换目录时省略 cd 命令，直接输入路径
     hist_ignore_all_dups    # 若命令重复则删除旧的命令记录
-    correct                 # 命令拼写自动纠错
 ) setopt $option
 
 # Tmux 中按 CTRL+S 会锁死
