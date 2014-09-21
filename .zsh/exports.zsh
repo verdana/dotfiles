2c87c02a99f56532b4c21f5d854b147b773a4091
Verdana Mu <verdana.cn@gmail.com>
2014-09-21 21:31:21 +0800

    禁止生成 MySQL 命令记录

--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -5,6 +5,9 @@ export EDITOR="`whence vim`"
 # 设定 TERM
 export TERM="xterm-256color"
 
+# 禁用 MySQL 命令记录
+export MYSQL_HISTFILE="/dev/null"
+
 # 以下的路径中搜索可自动载入的函数
 fpath=( "$HOME/.zsh" $fpath)
 export FPATH
