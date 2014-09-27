aebc26651ae1e5f98a427a0d1d67af41196d9713
Verdana Mu <verdana.cn@gmail.com>
2014-09-27 12:14:46 +0800

    /usr/local/(s)bin 目录添加到 PATH 中

--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -8,6 +8,9 @@ export TERM="xterm-256color"
 # 禁用 MySQL 命令记录
 export MYSQL_HISTFILE="/dev/null"
 
+# /usr/local 加入到 PATH 中
+export PATH="/usr/local/bin:/usr/local/sbin:$PATH"
+
 # 以下的路径中搜索可自动载入的函数
 fpath=( "$HOME/.zsh" $fpath)
 export FPATH
