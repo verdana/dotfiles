19bd7976325cd5da9ecd03eccf90922e8b29bcc5
Verdana <verdana.cn@gmail.com>
2016-11-25 14:47:34 +0800

    清理环境变量

--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -5,23 +5,25 @@ export EDITOR="`whence vim`"
 # 设定 TERM
 export TERM="xterm-256color"
 
+# 禁用 LESS  命令记录
+export LESSHISTFILE="/dev/null"
+
 # 禁用 MySQL 命令记录
 export MYSQL_HISTFILE="/dev/null"
 
-# /usr/local 加入到 PATH 中
-export PATH="/usr/local/opt/ccache/libexec:/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:$PATH"
+# Golang 加入到 PATH 中
+export PATH="/usr/local/go/bin:$PATH"
 
 # 将 node_modules 目录加入到 PATH 中
 export PATH="$HOME/node_modules/.bin:$PATH"
-
-# JAVA CLASSPATH
-export CLASSPATH="${CLASSPATH}:$HOME/.classes"
 
 # 以下的路径中搜索可自动载入的函数
 fpath=( "$HOME/.zsh" $fpath)
 export FPATH
 
-export LC_COLLATE="C"
+# 语言
+export LANG=en_US.UTF-8
+export LC_COLLATE=C
 
 # vim: set fdm=marker ff=unix sw=4 ts=4 et:
 
