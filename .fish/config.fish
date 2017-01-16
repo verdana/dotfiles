905ac414be6bb0da87783d75ab757a370895475f
Verdana <verdana.cn@gmail.com>
2017-01-17 03:42:11 +0800

    Update config.fish

--- .fish/config.fish
+++ .fish/config.fish
@@ -1,5 +1,22 @@
+
 set -U EDITOR       vim
 
-set fish_function_path /root/.config/fish/functions/theme-pure $fish_function_path
-set fish_user_paths $fish_user_paths /usr/local/go/bin
+set fish_function_path  $HOME/.config/fish/functions/theme-pure $fish_function_path
+set fish_user_paths     $fish_user_paths /usr/local/go/bin
+
+# Change the prompt text
+set pure_symbol_prompt "~>"
+#set pure_symbol_git_down_arrow "v"
+#set pure_symbol_git_down_arrow "^"
+#set pure_symbol_git_dirty "!"
+#set pure_symbol_horizontal_bar "_"
+
+# # Change the colors
+#set pure_color_blue (set_color "1e00fd")
+#set pure_color_cyan (set_color "1e95fd")
+#set pure_color_gray (set_color "6c6c6c")
+#set pure_color_green (set_color "66ff66")
+#set pure_color_normal (set_color "000000")
+#set pure_color_red (set_color "f820ff")
+#set pure_color_yellow (set_color "1bc8c8")
 
