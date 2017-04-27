d644331e41bbc6135d7c77c9dd170dfc6a4975ed
Verdana <verdana.cn@gmail.com>
2017-04-27 19:41:28 +0800

    添加 ccache 到 PATH

--- .fish/config.fish
+++ .fish/config.fish
@@ -2,6 +2,7 @@
 #set -U EDITOR       vim
 
 set fish_function_path  $HOME/.config/fish/functions/theme-pure $fish_function_path
+set fish_user_paths     $fish_user_paths /usr/lib/ccache
 set fish_user_paths     $fish_user_paths /usr/local/go/bin
 
 # Change the prompt text
