6e81b1038e35f74f6e15b2d0cb6f10dfbe52fdfb
Verdana <verdana.cn@gmail.com>
2017-05-26 15:34:03 +0800

    Create symlinks for neovim on Windows

---
+++ neovim.bat
@@ -0,0 +1,11 @@
+@echo off
+
+set DOT_DIR=D:\Github\dotfiles
+
+rmdir   /S /Q   %USERPROFILE%\AppData\Local\nvim\
+mklink  /D      %USERPROFILE%\AppData\Local\nvim\           %DOT_DIR%\vim\
+
+mklink          %USERPROFILE%\AppData\Local\nvim\init.vim   %DOT_DIR%\vimrc
+mklink          %USERPROFILE%\AppData\Local\nvim\ginit.vim  %DOT_DIR%\vimrc-gui
+
+PAUSE
