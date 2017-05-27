c6c5a0982d0c240e26ad98ddb66a651fa80e56bf
Verdana <verdana.cn@gmail.com>
2017-05-27 20:29:56 +0800

    change nvim symlink

--- neovim.bat
+++ neovim.bat
@@ -2,8 +2,8 @@
 
 set DOT_DIR=D:\Github\dotfiles
 
-rmdir   /S /Q   %USERPROFILE%\AppData\Local\nvim\
-mklink  /D      %USERPROFILE%\AppData\Local\nvim\           %DOT_DIR%\vim\
+rmdir   /S /Q   %USERPROFILE%\AppData\Local\nvim
+mklink  /J      %USERPROFILE%\AppData\Local\nvim\           %DOT_DIR%\vim
 
 mklink          %USERPROFILE%\AppData\Local\nvim\init.vim   %DOT_DIR%\vimrc
 mklink          %USERPROFILE%\AppData\Local\nvim\ginit.vim  %DOT_DIR%\vimrc-gui
