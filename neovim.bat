f3b13b7f5511ac1a5eecf4d1a31665951962588b
Verdana <verdana.cn@gmail.com>
2017-07-03 18:28:41 +0800

    删除错误的符号链接

--- neovim.bat
+++ neovim.bat
@@ -5,11 +5,4 @@ set DOT_DIR=D:\Github\dotfiles
 RMDIR   /S /Q   %USERPROFILE%\AppData\Local\nvim
 MKLINK  /J      %USERPROFILE%\AppData\Local\nvim            %DOT_DIR%\config\nvim
 
-
-DEL             %USERPROFILE%\AppData\Local\nvim\init.vim
-MKLINK          %USERPROFILE%\AppData\Local\nvim\init.vim   %DOT_DIR%\config\nvim\init.vim
-
-DEL             %USERPROFILE%\AppData\Local\nvim\ginit.vim
-MKLINK          %USERPROFILE%\AppData\Local\nvim\ginit.vim  %DOT_DIR%\config\nvim\ginit.vim
-
 PAUSE
