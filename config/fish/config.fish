c1410d1926d704254fbdf89cdcbd60ea950e359c
Verdana <verdana.cn@gmail.com>
2019-04-06 19:52:55 +0800

    Update fish.config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,12 +5,12 @@ set -gx VISUAL      vim
 set -gx EDITOR      vim
 
 # Hadoop
-set -gx HADOOP_HOME                     $HOME/hadoop-2.7.6
-set -gx HADOOP_COMMON_LIB_NATIVE_DIR    $HADOOP_HOME/lib/native
-set -gx HADOOP_OPTS                     "-Djava.library.path=$HADOOP_HOME/lib:$HADOOP_COMMON_LIB_NATIVE_DIR"
-set -gx JAVA_HOME                       "/usr/lib/jvm/java-8-openjdk"
-set -gx SCALA_HOME                      "/usr/share/scala"
-set -gx LD_LIBRARY_PATH                 "$HADOOP_HOME/lib/native:$LD_LIBRARY_PATH"
+#set -gx HADOOP_HOME                     $HOME/hadoop-2.7.6
+#set -gx HADOOP_COMMON_LIB_NATIVE_DIR    $HADOOP_HOME/lib/native
+#set -gx HADOOP_OPTS                     "-Djava.library.path=$HADOOP_HOME/lib:$HADOOP_COMMON_LIB_NATIVE_DIR"
+#set -gx JAVA_HOME                       "/usr/lib/jvm/java-8-openjdk"
+#set -gx SCALA_HOME                      "/usr/share/scala"
+#set -gx LD_LIBRARY_PATH                 "$HADOOP_HOME/lib/native:$LD_LIBRARY_PATH"
 
 # Pure prompt
 set fish_function_path $HOME/.config/fish/functions/theme-pure/functions $fish_function_path
@@ -24,9 +24,7 @@ set user_paths $user_paths              \
     /usr/lib/ccache                     \
     /usr/local/cmake-3.9.0/bin          \
     /usr/local/go/bin                   \
-    /usr/local/php-7.2/bin              \
-    /usr/local/sbt/bin                  \
-    /usr/share/scala/bin
+    /usr/local/php-7.2/bin
 
 for path in $user_paths
     if test -d $path
@@ -35,11 +33,11 @@ for path in $user_paths
 end
 
 # Change the prompt text
-#set pure_symbol_prompt         ">"
-#set pure_symbol_git_up_arrow   "^"
-#set pure_symbol_git_down_arrow "v"
-#set pure_symbol_git_dirty      "!"
-#set pure_symbol_horizontal_bar "_"
+set pure_symbol_prompt         ">"
+set pure_symbol_git_up_arrow   "^"
+set pure_symbol_git_down_arrow "v"
+set pure_symbol_git_dirty      "!"
+set pure_symbol_horizontal_bar "_"
 
 # Change the colors
 set pure_color_blue     (set_color "6496D2")
@@ -69,7 +67,6 @@ set pure_separate_prompt_on_error 1
 
 # Max execution time of a process before its run time is shown when it exits
 set pure_command_max_exec_time 5
-
 
 # Poatage
 alias eu='sudo etc-update'
