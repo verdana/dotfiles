7691966da779cdbb1b4451ad6b5a42762ef93488
Verdana <verdana.cn@gmail.com>
2019-08-24 17:12:01 +0800

    Remove unused file

--- neovim.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set DOT_DIR=D:\Github\dotfiles
-
-RMDIR   /S /Q   %USERPROFILE%\AppData\Local\nvim
-MKLINK  /J      %USERPROFILE%\AppData\Local\nvim            %DOT_DIR%\config\nvim
-
-PAUSE
