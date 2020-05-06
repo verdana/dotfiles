e82cf95a6468b671995719987ba576253fbb973a
Verdana <verdana.cn@gmail.com>
2020-05-06 22:07:47 +0800

    Cleanup

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -6,17 +6,27 @@ set -gx LC_COLLATE  C
 set -gx VISUAL      vim
 set -gx EDITOR      vim
 
-set -gx HOMEBREW_BOTTLE_DOMAIN  https://mirrors.ustc.edu.cn/homebrew-bottles
+# Homebrew
+if command -sq brew
+    set -gx HOMEBREW_BOTTLE_DOMAIN  https://mirrors.ustc.edu.cn/homebrew-bottles
+end
 
-export PUB_HOSTED_URL=https://pub.flutter-io.cn
-export FLUTTER_STORAGE_BASE_URL=https://storage.flutter-io.cn
+# flutter china mirror
+if command -sq flutter
+    set -gx PUB_HOSTED_URL           https://pub.flutter-io.cn
+    set -gx FLUTTER_STORAGE_BASE_URL https://storage.flutter-io.cn
+end
 
 # php composer
-set -gx COMPOSER_MEMORY_LIMIT     -1
+if command -sq composer
+    set -gx COMPOSER_MEMORY_LIMIT -1
+end
 
 # ccache
-set -gx CCACHE_PATH     /usr/bin
-set -gx CCACHE_DIR      /tmp/ccache
+if command -sq ccache
+    set -gx CCACHE_PATH /usr/bin
+    set -gx CCACHE_DIR  /tmp/ccache
+end
 
 # Pure prompt
 set fish_function_path $HOME/.config/fish/functions/theme-pure/functions $fish_function_path
@@ -25,18 +35,16 @@ source $HOME/.config/fish/functions/theme-pure/conf.d/pure.fish
 # User paths
 set user_paths $user_paths              \
     $HOME/.cargo/bin                    \
-    $HOME/.config/composer/vendor/bin   \
     $HOME/.composer/vendor/bin          \
     $HOME/.local/bin                    \
     $HOME/.rbenv/bin                    \
     $HOME/.fastlane/bin                 \
-    $HOME/go/bin                        \
     $HOME/flutter/bin                   \
     /usr/lib/ccache                     \
     /usr/local/clang-9.0.0/bin          \
     /usr/local/go/bin                   \
-    /usr/local/sbin                     \
-    /usr/local/opt/ruby/bin
+    /usr/local/opt/ruby/bin             \
+    /usr/local/sbin
 
 for path in $user_paths
     if test -d $path
@@ -56,56 +64,5 @@ set pure_right_prompt                   " "
 set pure_symbol_git_unpulled_commits    "➘"
 set pure_symbol_git_unpushed_commits    "➚"
 set pure_symbol_git_dirty               "✲"
-set pure_symbol_title_bar_separator     "—"
-
-# Change the colors
-# set pure_color_primary  (set_color blue)
-# set pure_color_info     (set_color cyan)
-# set pure_color_mute     (set_color brblack)
-# set pure_color_success  (set_color magenta)
-# set pure_color_normal   (set_color normal)
-# set pure_color_danger   (set_color red)
-# set pure_color_light    (set_color white)
-# set pure_color_warning  (set_color yellow)
-# set pure_color_dark     (set_color black)
-
-# Change colors for username and host in SSH
-#set pure_username_color $pure_color_yellow
-#set pure_host_color     $pure_color_green
-#set pure_root_color     $pure_color_red
-
-# Change where the username and host is displayed
-# 0 - end of prompt, default
-# 1 - start of prompt
-# Any other value defaults to the default behaviour
-set pure_user_host_location 0
-
-# Show exit code of last command as a separate prompt character.
-# As described here:
-# https://github.com/sindresorhus/pure/wiki#show-exit-code-of-last-command-as-a-separate-prompt-character
-#
-# 0 - single prompt character, default
-# 1 - separate prompt character
-# Any other value defaults to the default behaviour
-set pure_separate_prompt_on_error 1
-
-# Max execution time of a process before its run time is shown when it exits
-set pure_command_max_exec_time 5
-
-# Poatage
-alias eu='sudo etc-update'
-alias es='sudo eix-sync'
-alias em='sudo emerge --ask'
-# Display package information
-alias em-info 'sudo emerge --info'
-# Rebuilding your entire system
-alias em-update='sudo emerge -uDNav @world'
-# Remove package
-alias em-remove='sudo emerge --unmerge'
-# Removing obsoleted packages
-alias em-clean='sudo emerge --depclean'
-# Search a package
-alias em-search 'sudo emerge --search'
-# Running revdep-rebuild
-alias rr='sudo revdep-rebuild -p'
+set pure_symbol_title_bar_separator     "-"
 
