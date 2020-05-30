7384d73ef7fcea5999f3d15249e2c6c011fd8434
Verdana <verdana.cn@gmail.com>
2020-05-30 22:29:39 +0800

    Update grep path in linux/darwin

--- fish/functions/grep.fish
+++ fish/functions/grep.fish
@@ -1,4 +1,8 @@
 function grep
-    /usr/bin/grep -In --color=always --exclude-dir=.svn --exclude-dir=.git $argv
+    switch (uname)
+        case Linux
+            /bin/grep -In --color=always --exclude-dir=.svn --exclude-dir=.git $argv
+        case Darwin
+            /usr/bin/grep -In --color=always --exclude-dir=.svn --exclude-dir=.git $argv
+    end
 end
-
