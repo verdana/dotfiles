a2a9c2fd36c35cf52453162df4b88975603c3e39
Verdana <verdana.cn@gmail.com>
2020-07-20 15:08:25 +0800

    PROXY

--- fish/functions/proxy.fish
+++ fish/functions/proxy.fish
@@ -2,5 +2,6 @@ function proxy
     export all_proxy="http://127.0.0.1:1087"
     export http_proxy="http://127.0.0.1:1087"
     export https_proxy="http://127.0.0.1:1087"
+    export no_proxy="localhost,*.app,192.168.*.*"
     env
 end
--- fish/functions/unproxy.fish
+++ fish/functions/unproxy.fish
@@ -2,5 +2,6 @@ function unproxy
     set -e all_proxy
     set -e http_proxy
     set -e https_proxy
+    set -e no_proxy
     env
 end
