044925655d4ce1a35717928e53cb9b1fe7e96522
Verdana <verdana.cn@gmail.com>
2020-08-13 15:28:23 +0800

    修改代理命令端口

--- fish/functions/proxy.fish
+++ fish/functions/proxy.fish
@@ -1,7 +1,7 @@
 function proxy
-    export all_proxy="http://127.0.0.1:1087"
-    export http_proxy="http://127.0.0.1:1087"
-    export https_proxy="http://127.0.0.1:1087"
-    export no_proxy="localhost,*.app,192.168.*.*"
+    export http_proxy="http://127.0.0.1:7890"
+    export https_proxy="http://127.0.0.1:7890"
+    export all_proxy="socks5://127.0.0.1:7890"
+    export no_proxy="localhost,127.0.0.1,*.app,192.168.*.*"
     env
 end
