fe360656a5f74ff9e7f4567e4d33b5dd3e669f37
Verdana <verdana.cn@gmail.com>
2020-12-14 10:47:42 +0800

    fix grep path

--- fish/config.fish
+++ fish/config.fish
@@ -75,7 +75,7 @@ if command -sq ccache
 end
 
 # If wsl, go to home directory
-if /bin/grep -iqs microsoft /proc/sys/kernel/osrelease
+if grep -iqs microsoft /proc/sys/kernel/osrelease
     cd $HOME
 end
 
