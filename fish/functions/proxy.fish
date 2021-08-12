ea35643b6dfd089e8189940f02c5d8d167826a79
Verdana <verdana.cn@gmail.com>
2021-08-12 23:40:11 +0800

    fix proxy error

--- fish/functions/proxy.fish
+++ fish/functions/proxy.fish
@@ -1,11 +1,13 @@
 function proxy
     # 设定默认的代理服务器
-    set proxy "127.0.0.1:7890"
+    set host_addr "127.0.0.1"
 
     # 是不是 wsl 环境
     if grep -iqs microsoft /proc/sys/kernel/osrelease
-        set proxy (/bin/grep -oP '(?<=nameserver\ ).*' /etc/resolv.conf)
+        set host_addr (/bin/grep -oP '(?<=nameserver\ ).*' /etc/resolv.conf)
     end
+
+    set proxy "$host_addr:7890"
 
     # 设定全局代理
     export http_proxy="http://$proxy"
