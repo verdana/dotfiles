16484bf0760da5915d15b33c48ebe84584cb7291
Verdana <verdana.cn@gmail.com>
2021-08-18 10:29:13 +0800

    update JAVA env

--- fish/java+android.fish
+++ fish/java+android.fish
@@ -2,9 +2,15 @@
 # Java & Android
 # do not use ANDROID_HOME anymore
 
-#set -gx JAVA_HOME          "/usr/local/Cellar/openjdk@8/1.8.0+282/libexec/openjdk.jdk/Contents/Home"
-#set -gx JAVA_HOM           "/Applications/Android Studio.app/Contents/jre/jdk/Contents/Home"
-set -gx JAVA_HOME           "/Library/Java/JavaVirtualMachines/amazon-corretto-8.jdk/Contents/Home"
+# MacOS
+if test (uname -s) = "Darwin"
+    set -gx JAVA_HOME           "/Library/Java/JavaVirtualMachines/amazon-corretto-8.jdk/Contents/Home"
+    set -gx ANDROID_SDK_ROOT    "$HOME/Galerida/Android/SDK"
+end
 
-set -gx ANDROID_SDK_ROOT    "$HOME/Galerida/Android/SDK"
+# Arch
+if grep -iqs arch /proc/sys/kernel/osrelease
+    set -gx JAVA_HOME   "/usr/lib/jvm/java-8-openjdk"
+end
+
 
