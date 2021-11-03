32eeec19a3ccf5b0ceb7bcacb293c66a4d4ab0bb
Verdana <verdana.cn@gmail.com>
2021-11-03 17:15:56 +0800

    默认使用 Java11

--- fish/java+android.fish
+++ fish/java+android.fish
@@ -4,7 +4,7 @@
 
 # MacOS
 if test (uname -s) = "Darwin"
-    set -gx JAVA_HOME           "/Library/Java/JavaVirtualMachines/amazon-corretto-8.jdk/Contents/Home"
+    set -gx JAVA_HOME           "/Library/Java/JavaVirtualMachines/amazon-corretto-11.jdk/Contents/Home"
     set -gx ANDROID_SDK_ROOT    "$HOME/Galerida/Android/SDK"
 end
 
