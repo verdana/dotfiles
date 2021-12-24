f40478dde2f5dfa6a9612ef4b4ac3e948b9129e2
Verdana <verdana.cn@gmail.com>
2021-12-24 10:31:03 +0800

    fix: 修正代理协议问题

--- fish/functions/proxy.fish
+++ fish/functions/proxy.fish
@@ -17,13 +17,13 @@ function proxy
     # 设定全局代理
     export http_proxy="http://$proxy"
     export https_proxy="http://$proxy"
-    export all_proxy="socks5://$proxy"
+    export all_proxy="http://$proxy"
     export no_proxy="localhost,127.0.0.1,*.app,192.168.*.*"
     echo "set global proxy: $proxy"
 
     # 设定 git 代理
-    git config --global http.proxy  "socks5://$proxy"
-    git config --global https.proxy "socks5://$proxy"
+    git config --global http.proxy  "http://$proxy"
+    git config --global https.proxy "http://$proxy"
     echo "set git proxy: $proxy"
 
     # 设定 npm 代理
