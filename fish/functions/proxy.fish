9afcd90091dfc1df2c403f484d188eeee8318553
Verdana <verdana.cn@gmail.com>
2022-02-10 11:47:50 +0800

    追加 ::1 到 no_proxy

--- fish/functions/proxy.fish
+++ fish/functions/proxy.fish
@@ -18,7 +18,7 @@ function proxy
     export http_proxy="http://$proxy"
     export https_proxy="http://$proxy"
     export all_proxy="http://$proxy"
-    export no_proxy="localhost,127.0.0.1,*.app,192.168.*.*"
+    export no_proxy="localhost,127.0.0.1,::1,*.app,192.168.*.*"
     echo "set global proxy: $proxy"
 
     # 设定 git 代理
