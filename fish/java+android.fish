2c5a43093987430aaeb7a82af750051eb465d790
Verdana <verdana.cn@gmail.com>
2022-05-02 15:18:37 +0800

    重写安卓SDK位置

--- fish/java+android.fish
+++ fish/java+android.fish
@@ -5,12 +5,11 @@
 # MacOS
 if test (uname -s) = "Darwin"
     set -gx JAVA_HOME           "/Library/Java/JavaVirtualMachines/amazon-corretto-11.jdk/Contents/Home"
-    set -gx ANDROID_SDK_ROOT    "$HOME/Galerida/Android/SDK"
+    set -gx ANDROID_SDK_ROOT    "/Volumes/Basaltic/Android/SDK"
 end
 
 # Arch
 if grep -iqs arch /proc/sys/kernel/osrelease
     set -gx JAVA_HOME   "/usr/lib/jvm/java-11-openjdk"
 end
-
 
