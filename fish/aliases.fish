a90900414cfd925b18a35623305646a4d84da91d
Verdana <verdana.cn@gmail.com>
2022-05-17 11:35:32 +0800

    添加更多的别名命令

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -6,11 +6,30 @@ function ....  ; cd ../../.. ; end
 function ..... ; cd ../../../.. ; end
 
 # shortcuts
-alias svc='supervisorctl'
 alias df='df -P -klH'
-alias ar2='aria2c'
-alias push='git push'
-alias master='git co master'
-alias trunk='git co trunk'
+
+# apt
 alias apt-proxy='sudo apt-get -c ~/.apt_proxy'
+
+# aria2c
+if command -sq aria2c
+    alias ar2='aria2c'
+end
+
+# git
+if command -sq git
+    alias push='git push'
+    alias master='git co master'
+    alias trunk='git co trunk'
+end
+
+# nvim
+if command -sq nvim
+    alias vim=nvim
+end
+
+# lazygit
+if command -sq lazygit
+    alias lg=lazygit
+end
 
