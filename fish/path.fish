db745f485a9efccae7e3f410693dd6b421b7fbdb
Verdana <verdana.cn@gmail.com>
2022-11-16 17:01:04 +0800

    fix: 调整 fish_user_paths 中的路径顺序

--- fish/path.fish
+++ fish/path.fish
@@ -1,22 +1,21 @@
 
 # User paths
-set user_paths $user_paths          \
-    $HOME/.cargo/bin                \
-    $HOME/.composer/vendor/bin      \
-    $HOME/.local/bin                \
-    $HOME/.pyenv/bin                \
-    $HOME/.rbenv/bin                \
-    $HOME/flutter/bin               \
-    $HOME/go/bin                    \
-    /usr/local/cmake/bin            \
-    /usr/local/go/bin               \
-    /usr/local/opt/mysql-client/bin \
-    /usr/local/sbin                 \
-    /usr/lib/ccache                 \
+set user_paths $user_paths      \
+    /usr/local/bin              \
+    /usr/local/sbin             \
+    /usr/local/cmake/bin        \
+    /usr/local/go/bin           \
+    /usr/lib/ccache             \
+    $HOME/.cargo/bin            \
+    $HOME/.composer/vendor/bin  \
+    $HOME/.local/bin            \
+    $HOME/.pyenv/bin            \
+    $HOME/.rbenv/bin            \
+    $HOME/flutter/bin           \
+    $HOME/go/bin                \
 
 for path in $user_paths
     if test -d $path
-        # set -x PATH $path $PATH
-        fish_add_path -U $path
+        fish_add_path -g $path
     end
 end
